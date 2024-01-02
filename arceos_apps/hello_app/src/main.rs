bc107e8f543b4c21fb853d85dba40907ece3276b
LiuZhetan <liuzhetande@foxmail.com>
2024-01-02 18:15:12 +0800

    add app for arceos

---
+++ arceos_apps/hello_app/src/main.rs
@@ -0,0 +1,70 @@
+#![feature(asm_const)]
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
+
+#[inline]
+fn get_abi_addr() -> usize {
+    let abi_tb_addr: usize;
+    unsafe {
+        core::arch::asm!(
+            "mv {}, a7", 
+            out(reg) abi_tb_addr,
+        )
+    };
+    abi_tb_addr
+}
+
+fn putchar(abi_tb_addr: usize, c: u8) {
+    // do not use variable to directly replace register t2
+    unsafe {
+        core::arch::asm!("
+        li     t0, {abi_num}
+        slli   t0, t0, 3
+        add     t1, t0, t2
+        ld     t1, (t1)
+        jalr   t1",
+        abi_num = const SYS_PUTCHAR,
+        in("t2") abi_tb_addr,
+        in("a0") c,
+        )
+    }
+}
+
+// must add no_mangle
+#[no_mangle]
+fn puts(abi_tb_addr: usize, s: &str) {
+    for c in s.bytes() {
+        putchar(abi_tb_addr, c);
+    }
+}
+
+fn shutdown(abi_tb_addr:usize) -> ! {
+    unsafe {
+        core::arch::asm!("
+        li     t0, {abi_num}
+        slli   t0, t0, 3
+        add     t1, {addr}, t0
+        ld     t1, (t1)
+        jalr   t1",
+        abi_num = const SYS_TERMINATE,
+        addr = in(reg) abi_tb_addr,
+        options(noreturn),
+        )
+    }
+}
+
+#[no_mangle]
+unsafe extern "C" fn _start() -> ! {
+    let abi_tb_addr = get_abi_addr();
+    puts(abi_tb_addr, "D");
+    shutdown(abi_tb_addr)
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
